478722530f549a16c12994e2d80df760acaaebf7
hectordev14 <0101diaz.hector@gmail.com>
2025-11-27 21:41:29 -0600

    desplegando en netlify

--- src/routes/login/index.tsx
+++ src/routes/login/index.tsx
@@ -1,13 +1,63 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { supabase } from '../../supabase/client'
+import React, { useState } from 'react'
 
 export const Route = createFileRoute('/login/')({
     component: Index,
 })
 
+
+
 function Index() {
+    interface Data{
+        email: string ;
+        password: string ;
+    }
+    const [data, setData] = useState<Data>({
+        email: "",
+        password: ""
+    });
+    
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault()
+        await supabase.auth.signUp({
+            email: data.email,
+            password: data.password,
+        })
+        await supabase
+            .from('profiles')
+            .insert({
+                email: data.email,
+                password: data.password,
+            })
+
+    }
     return (
+        <>
         <div className="p-2">
             <h3>Inicia Sesion</h3>
         </div>
+        <form>
+                <input
+                    type="text"
+                    name='email'
+                    placeholder='youremail@site.com'
+                    onChange={(e) => setData({ ...data, email: e.target.value })}
+                />
+            <input 
+            type="text"
+            name='email'
+            placeholder='youremail@site.com'
+            onChange={(e) => setData({ ...data, email: e.target.value})}
+            />
+                <input
+                    
+                    type="password"
+                    name='password'
+                    onChange={(e) => setData({ ...data, password: e.target.value })}
+                />
+            <button onClick={handleSubmit}>Enviar</button>
+        </form>
+        </>
     )
 }
